f1075b95dfe7d631e0dbaf865c3927d8bf9ce34b
tomnoal123 <tomnoal123@gmail.com>
2026-05-04 22:23:11 +0200

    Update siginUserDataDownloadByLocalStorage.js

--- todo-app.html/logowanie/siginUserDataDownloadByLocalStorage.js
+++ todo-app.html/logowanie/siginUserDataDownloadByLocalStorage.js
@@ -3,38 +3,29 @@
 const form = document.getElementById('user-data-form');
 const signinButton = document.getElementById('sign-button');
 const checkbox = document.getElementById('checkbox');
-
-
-
-
 function userInfo (){
 const userData = {};
-const inputs = document.querySelectorAll('input');
+const inputs = form.querySelectorAll('input');
 
-
-inputs.forEach((input) =>{
-    if (input.type !== "checkbox") {
-    userData.push(input.value)
+    inputs.forEach((input) =>{
+        if (input.type !== "checkbox") {        
+    
+        userData[input.name] = input.value;
+    /* WEAKER OPTION userData.push(input.value)*/
 }});
     localStorage.setItem("userData", JSON.stringify(userData));
     return userData;
 };
 
-
-
-
-signinButton.addEventListener("click", function(){
-    const data = userInfo();
-    const saved = JSON.parse(localStorage.getItem("userData"));
-    console.log(saved);
-})
-
-
 form.addEventListener("submit", function(f){
     if(!checkbox.checked){
         f.preventDefault();
-        alert("Musisz zaakcpetować regulamin");
+        alert("Musisz zaakceptować regulamin");
     }
+    const data = userInfo();
+    const saved = JSON.parse(localStorage.getItem("userData"));
 })
+
+
 
 
