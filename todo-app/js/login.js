14a38cb52cafdc5ad8fc6731b1bea00b1cbeeae0
tomnoal123 <tomnoal123@gmail.com>
2026-05-08 21:58:19 +0200

    Refactor login validation to use find method

--- todo-app/js/login.js
+++ todo-app/js/login.js
@@ -1,30 +1,34 @@
 import { getUserData } from "./storage.js";
 
 const form = document.getElementById('main-login-form');
-if (form){
-const inputUsername = document.getElementById('input-username');
-const inputPassword = document.getElementById('input-password');
-const checkBox = document.getElementById('login-checkbox');
 
-form.addEventListener("submit", function(e){
-    e.preventDefault();
-    const userData = getUserData();
+if (form) {
+    const inputUsername = document.getElementById('input-username');
+    const inputPassword = document.getElementById('input-password');
+    const checkBox = document.getElementById('login-checkbox');
 
-    if (
-        inputUsername.value !== userData.username ||inputPassword.value !== userData.password) {
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
 
-        alert('Niepoprawne dane');
-        return;
-    }
-    if(checkBox.checked){
-    
-    localStorage.setItem("RememberUserData", JSON.stringify({
-        username: inputUsername.value,
-        password: inputPassword.value
-    }));
-    };
-   window.location.href = "todoapp.html";
-})
+        const users = getUserData();
+
+        const user = users.find(u =>
+            u.username === inputUsername.value &&
+            u.password === inputPassword.value
+        );
+
+        if (!user) {
+            alert("Niepoprawne dane");
+            return;
+        }
+
+        if (checkBox.checked) {
+            localStorage.setItem("RememberUserData", JSON.stringify({
+                username: inputUsername.value,
+                password: inputPassword.value
+            }));
+        }
+
+        window.location.href = "todoapp.html";
+    });
 }
-    
-
